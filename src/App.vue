0428563017bb4ceb91658f317b2118f727c4ed52
brandon-schabel <brandonschabel1995@gmail.com>
2018-02-06 18:58:43 -0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <pp-header></pp-header>
+    </div>
+    <div class="container">
+      <pp-admin></pp-admin>
+    </div>
+    <div class="container">
+      <pp-footer></pp-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Admin from './components/Admin'
+
+export default {
+  components: {
+    ppHeader: Header,
+    ppFooter: Footer,
+    ppHome: Home,
+    ppMenu: Menu,
+    ppAdmin: Admin
+  }
+}
+</script>
+
+<style>
+</style>
