dca62a38de4d6412b49f2c67244f0606857da515
brandon-schabel <brandonschabel1995@gmail.com>
2018-02-07 18:00:17 -0800

    configure firebase

---
+++ src/firebaseConfigExample.js
@@ -0,0 +1,14 @@
+// zremove the Example part from the name and configure file accordingly
+// this is just an exaple, fill in your own data from firebase
+import Firebase from 'firebase'
+var config = {
+  apiKey: "3930483043824dfjdh32k4h3434",
+  authDomain: "notarealapp.firebaseapp.com",
+  databaseURL: "https://notarealapp.firebaseio.com",
+  projectId: "notarealapp",
+  storageBucket: "notarealapp.appspot.com",
+  messagingSenderId: "98793740732439847324"
+};
+const firebaseApp = Firebase.initializeApp(config);
+const db = firebaseApp.database(); // creating a reference to the database.
+const dbMenuRef = db.ref('menu') // reference to menu collection in the database
