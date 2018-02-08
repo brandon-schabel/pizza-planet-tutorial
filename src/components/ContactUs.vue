492d7b0914428e63682fa9a5e3659f495c20f3d2
brandon-schabel <brandonschabel1995@gmail.com>
2018-02-07 21:52:00 -0800

    Add boilerplaate components

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">Card title</h4>
+      <p class="card-text">
+        <address>
+          <strong>Pizza Planet</strong><br>
+          Address Line 1<br>
+          Address Line 2<br>
+          Phone: (123) 456-7890
+        </address>
+
+        <address>
+          <strong>Email:</strong>
+          <p>contact@pizzaplanet.com</p>
+        </address>
+      </p>
+    </div>
+  </div>
+</template>
---
+++ src/components/OrderingGuide.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">How To Order</h4>
+      <p class="card-text">
+        Some quick example text to build on the title............
+      </p>
+    </div>
+  </div>
+</template>
